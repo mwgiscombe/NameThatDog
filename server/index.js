91a3a9cf515d0daa13dab3f2e1dc9355dced0249
Madeline <mwgiscombe@gmail.com>
2026-05-27 11:13:16 -0700

    Add debug logging

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const userRoutes = require('./src/routes/user.routes')
 
 
 dotenv.config()
+console.log('MONGODB_URL exists:', !!process.env.MONGODB_URL)
+console.log('PORT:', process.env.PORT)
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended: false}))
